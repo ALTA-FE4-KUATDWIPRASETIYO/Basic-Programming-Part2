c3630b2e2bc35b0e183d918b5b69fbee098ced15
dwprstyo <kuatdwiprasetiyo@gmail.com>
2022-02-24 21:41:17 +0700

    coba

--- Part2/problem1/problem1.js
+++ Part2/problem1/problem1.js
@@ -1,7 +1,17 @@
-// input
-let studentScore = 80;
+let studentScore =  101
 
-// Process: Your Solution Code Here
-
-// Output
-// Nilai A
+if (studentScore < 0){
+    console.log('The number should be 0 - 100');
+}else if(studentScore <= 34){
+    console.log('D');
+}else if(studentScore <= 49){
+    console.log('C');
+}else if(studentScore <= 64){
+    console.log('B');
+}else if(studentScore <= 79){
+    console.log('B+');
+}else if(studentScore <= 100){
+    console.log('A');
+}else{
+    console.log('The number should be 0 - 100');
+}
